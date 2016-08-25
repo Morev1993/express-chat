926edb08f5dc160ff6b154e07c7d1893f784f27f
i.morev <i.morev@cy.cardpay.com>
2016-08-25 17:52:57 +1000

    add config, user-model and test operations with db

--- db.js
+++ db.js
@@ -1,8 +1,53 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+/*jshint esversion: 6 */
 
-var Cat = mongoose.model('Cat', { name: String });
+let co = require('co');
+let mongoose = require('./libs/mongoose');
 
-Cat.find({}, function (err, cats) {
-	console.log(cats);
-})
+mongoose.set('debug', true);
+
+function open() {
+	return new Promise((resolve, reject) => mongoose.connection.once('open', resolve) );
+}
+
+function dropDb() {
+	return new Promise((resolve, reject) => mongoose.connection.db.dropDatabase(resolve) );
+}
+
+function* createUsers() {
+	let User = require('./models/user').User;
+
+	yield new Promise((resolve, reject) => mongoose.models.User.on('index', resolve));
+
+	let userPromises = [
+	    { username: "Vasya", password: "secret1" },
+	    { username: "Petya", password: "secret2" },
+	    { username: "Dno", password: "secret3" }
+  ].map(userData => {
+	    let user = new User(userData);
+	    return user.save();
+  });
+
+  let users = yield Promise.all(userPromises);
+
+	return users;
+}
+
+function closeDb() {
+	mongoose.disconnect();
+	return Promise.resolve();
+}
+
+co(function *() {
+	try {
+		yield open();
+		yield dropDb();
+		let users = yield* createUsers();
+		console.log(users);
+	} catch(e) {
+		throw e;
+	} finally {
+		yield closeDb();
+	}
+
+	return "Done";
+}).then(value => console.log(value), err => console.log(err.stack));
