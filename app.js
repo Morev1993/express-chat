6b6d1b1340f635e247542ebbe6c497621824ad7a
i.morev <i.morev@cy.cardpay.com>
2016-09-27 16:55:53 +1000

    finished chat login

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(require('middleware/sendHttpError'));
+app.use(require('middleware/loadUser'));
+
 app.use('/', routes);
 
 // error handlers
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var HttpError = require('error').HttpError;
 var router = express.Router();
 var mongoose = require('libs/mongoose');
+var checkAuth = require('middleware/checkAuth')
 
 var User = require('models/user').User;
 
@@ -23,6 +24,7 @@ router.post('/login', function(req, res, next) {
   			console.log('Юзер существует: ' + user);
   			if (user.checkPassword(password)) {
   				console.log('Пароль верен');
+  				req.session.user = user._id;
   				res.send(user);
 	    	} else {
 	    		return next(new HttpError(403, 'Пароль неверен'));
@@ -46,9 +48,16 @@ router.post('/login', function(req, res, next) {
   	})
 });
 
-router.get('/chat', function(req, res, next) {
+
+router.post('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+router.get('/chat', checkAuth, function(req, res, next) {
   res.render('chat');
 });
+
 
 
 router.get('/users', function(req, res, next) {
